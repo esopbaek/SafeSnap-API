4166d10792c3be360c824f494a14639a58ae6685
Esop Baek <e.baek@celmatix.com>
2016-09-15 01:52:09 -0400

    use psql

--- SafeSnap/db/schema.rb
+++ SafeSnap/db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160911224233) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "image_sets", force: :cascade do |t|
     t.string   "name"
     t.string   "description"
